cdc0db7725ff6e8cdfe8245a67c00147eb6234d2
Noah Glushien <nglushien@techtarget.com>
2017-06-12 22:04:57 -0400

    Modified the chart and the css

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Info Graphic
 
-This repo is being used to work through the info graphic challenge. 
+This repo is being used to work through the info graphic challenge.
+
+## Explanation of Approach
+
+I was always taught not to reinvent the wheel so I decided work with ChartJS on this project. The JS plugin will allow me to easily scale this element to use imported data. 
+
+By modifing the chart object properties I'm able to modify the design to match the layout provided in the animated gif. I'm also able to easily animate the graph without having to animate keyframes of each bar.
+
+I believe this is also a good use of interactive data since the product team has decided to incorporate the chart to engage new users. The user can hover their cursor over each piece of data and get exact values.
+
+While this is a proof of concept I'd like to note in a Production ready environment I'd strive for pixel perfection when matching the approved design.
--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
-	var ctx = document.getElementById("myChart");
+	var ctx = document.getElementById("myChart"),
+		currentValue,
+		data;
+
 	var myChart = new Chart(ctx, {
 		type: 'bar',
 		data: {
-			labels: ["May 2016", "", "", "", "", "", "", "", "", "", "", "May 2017",],
+			labels: ["May 2016", "", "", "", "", "", "", "", "", "", "", "May 2017"],
 			datasets: [{
 				label: '7372 available articles',
 				data: [1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000, 5500, 6000, 6500, 7372],
@@ -23,22 +26,45 @@ $(document).ready(function() {
 				]
 			}]
 		},
+		maintainAspectRatio: false,
 		options: {
 			scales: {
 				yAxes: [{
 					ticks: {
-						beginAtZero:true
+						beginAtZero: true
 					},
 					gridLines: {
-						display: false
+						display: false,
+						color: '#b6e1f4',
+						lineWidth: 3
 					}
 				}],
 				xAxes: [{
 					gridLines: {
-						display: false
-					}
+						display: false,
+						color: '#b6e1f4',
+						lineWidth: 3
+					},
+					barPercentage: 0.4
 				}]
+			},
+			legend: {
+				display: false
+			},
+			tooltips: {
+				callbacks: {
+					label: function(tooltipItem) {
+						return tooltipItem.yLabel;
+					}
+				}
 			}
 		}
 	});
+
+	console.log(myChart);
+
+	data = myChart.data.datasets[0].data;
+	currentValue = Math.max.apply(null, data);
+	
+	$('<h1><span>' + currentValue + '</span> available articles</h1>').prependTo('#chart_container');
 });
